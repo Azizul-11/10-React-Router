528e36563c47a2a9cbc22e31e71271f2e3dac0e0
Md Azizul Islam <mdazizul8741@gmail.com>
2024-06-04 18:51:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import { React } from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+
+const App = () => {
+  return (
+   <>
+    <Header/>
+    <Footer/>
+    <Home/>
+
+   </>
+  )
+}
+
+export default App
